1eaac3debfffe63981653ae5b14b9fa27992ee95
dialdart <martin.dialedart@gmail.com>
2022-09-04 14:37:48 -0300

    Creo carpeta back

---
+++ PI_AP/BackEnd/src/test/java/com/portfolio/mad/MadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
